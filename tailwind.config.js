27d60ad3af1c676338bc11dfcbc35d9ed9312c50
mrsherpacode <sherpacoding@gmail.com>
2025-09-02 21:30:58 -0500

    some tailwind styles are override and cofigured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// This file is for configuration and we can override tailwind styles.
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Roboto Mono", "monospace"],
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
