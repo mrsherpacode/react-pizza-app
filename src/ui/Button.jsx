3a7884d2be644514a43ff3f567ddb53fbe4cdfd0
mrsherpacode <sherpacoding@gmail.com>
2025-09-05 15:26:21 -0500

    more styles are added

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 // creating a reusable Button component
 function Button({ children, disabled, to, typeOf }) {
   const base =
-    "inline-block rounded-full bg-yellow-400  uppercase tracking-wide text-stone-600 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "inline-block  text-sm rounded-full bg-yellow-400  uppercase tracking-wide text-stone-600 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
-    base: base + " px-4  py-2 md:px-5 md:px-2.5 ",
+    base: base + " px-4 py-2 md:px-5 md:py-2.5",
+    secondary:
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   if (to)
     return (
@@ -15,7 +17,7 @@ function Button({ children, disabled, to, typeOf }) {
       </Link>
     );
   return (
-    <button to={to} disabled={disabled} className={styles[typeOf]}>
+    <button disabled={disabled} className={styles[typeOf]}>
       {children}
     </button>
   );
